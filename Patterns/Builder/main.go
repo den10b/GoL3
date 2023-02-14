71c55e6900871f4ac7d64a2b2539ba9e8c59650c
den10b <den10b@yandex.ru>
2023-02-14 20:42:49 +0300

    пара паттернов

---
+++ Patterns/Builder/main.go
@@ -0,0 +1,105 @@
+package main
+
+import "fmt"
+
+type IChief interface {
+	setBread()
+	setMeat()
+	setGreens()
+	getBurger() burger
+}
+
+func getChief(chief string) IChief {
+	if chief == "default" {
+		return newDefaultChief()
+	}
+
+	if chief == "vegan" {
+		return newVeganChief()
+	}
+	return nil
+}
+
+type burger struct {
+	bread  string
+	meat   string
+	greens string
+}
+
+type veganChief struct {
+	bread  string
+	meat   string
+	greens string
+}
+
+func newVeganChief() *veganChief {
+	return &veganChief{}
+}
+
+func (v *veganChief) setBread() {
+	v.bread = "Веганский хлеб"
+}
+func (v *veganChief) setMeat() {
+	v.meat = "Соевое мясо"
+}
+func (v *veganChief) setGreens() {
+	v.greens = "Много зелени"
+}
+func (v *veganChief) getBurger() burger {
+	return burger{bread: v.bread,
+		meat:   v.meat,
+		greens: v.greens}
+}
+
+type defaultChief struct {
+	bread  string
+	meat   string
+	greens string
+}
+
+func newDefaultChief() *defaultChief {
+	return &defaultChief{}
+}
+
+func (d *defaultChief) setBread() {
+	d.bread = "Белый хлеб"
+}
+func (d *defaultChief) setMeat() {
+	d.meat = "Говяжья котлета"
+}
+func (d *defaultChief) setGreens() {
+	d.greens = "Пара огурчиков"
+}
+func (d *defaultChief) getBurger() burger {
+	return burger{bread: d.bread,
+		meat:   d.meat,
+		greens: d.greens}
+}
+
+type Director struct {
+	chief IChief
+}
+
+func newChief(c IChief) *Director {
+	return &Director{
+		chief: c,
+	}
+}
+
+func (d *Director) setChief(c IChief) {
+	d.chief = c
+}
+
+func (d *Director) makeBurger() burger {
+	d.chief.setBread()
+	d.chief.setMeat()
+	d.chief.setGreens()
+	return d.chief.getBurger()
+}
+func main() {
+	burgerShop := newChief(getChief("vegan"))
+	veganBurger := burgerShop.makeBurger()
+	fmt.Println(veganBurger)
+	burgerShop.setChief(getChief("default"))
+	fmt.Println(burgerShop.makeBurger())
+}
---
+++ Patterns/Facade/main.go
@@ -0,0 +1,101 @@
+package main
+
+import "fmt"
+
+type shopFacade struct {
+	item Item
+	bank Bank
+}
+
+func newShopFacade(cardNum int) *shopFacade {
+	fmt.Println("Создаем аккаунт")
+	shopFacade := &shopFacade{
+		bank: findUser(cardNum),
+	}
+	fmt.Println("Аккаунт создан!")
+	return shopFacade
+}
+func (f *shopFacade) tryToBuy(itemId int) {
+	f.item = findItem(itemId)
+	enoughMoney := f.bank.checkMoney(f.item.price)
+	if enoughMoney {
+		f.bank.spendMoney(f.item.price)
+		f.item.buy()
+		println("Успешно куплено")
+	} else {
+		println("Денег не хватает")
+	}
+
+}
+
+type Item struct {
+	price    int
+	itemID   int
+	quantity int
+}
+
+func findItem(itemID int) Item {
+	item := new(Item)
+	switch itemID % 10 {
+	case 1:
+		item.itemID = itemID
+		item.price = 10
+		item.quantity = 5
+	case 2:
+		item.itemID = itemID
+		item.price = 15
+		item.quantity = 0
+	case 3:
+		item.itemID = itemID
+		item.price = 100
+		item.quantity = 1
+	default:
+		item.itemID = itemID
+		item.price = 100000
+		item.quantity = 0
+	}
+	return *item
+}
+func (i *Item) buy() {
+	i.quantity = -1
+}
+
+type Bank struct {
+	bankID     int
+	userCardID int
+	userWallet int
+}
+
+func findUser(cardNum int) Bank {
+	bank := new(Bank)
+	switch cardNum % 10 {
+	case 1:
+		bank.bankID = 101
+		bank.userCardID = cardNum
+		bank.userWallet = 50
+
+	case 2:
+		bank.bankID = 202
+		bank.userCardID = cardNum
+		bank.userWallet = 20
+	default:
+		bank.bankID = 123
+		bank.userCardID = cardNum
+		bank.userWallet = 0
+	}
+	return *bank
+}
+func (b *Bank) checkMoney(val int) bool {
+	return val <= b.userWallet
+}
+func (b *Bank) spendMoney(val int) {
+	b.userWallet -= val
+}
+
+func main() {
+	adidas := newShopFacade(121)
+	adidas.tryToBuy(111)
+	nike := newShopFacade(122)
+	nike.tryToBuy(113)
+
+}
